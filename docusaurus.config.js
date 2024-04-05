4106748ba93e300dca68c06418530d1bb349e765
durulkoca <durulkoca@gmail.com>
2024-04-06 02:10:39 +0900

    Enable dark mode switch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,8 +49,8 @@ const config = {
     ({
       colorMode: {
         defaultMode: "light",
-        disableSwitch: true,
-        respectPrefersColorScheme: false,
+        disableSwitch: false,
+        respectPrefersColorScheme: true,
       },
       navbar: {
         items: [
